d64dde28136656e1fbb382d2827fd1549bd048ff
Riski Agung <riski97@gmail.com>
2022-03-02 07:32:07 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import React, { useState, useEffect } from "react";
+import { fabric } from "fabric";
 
 function App() {
+  const [canvas, setCanvas] = useState("");
+
+  const initCanvas = () => {
+    new fabric.Canvas("canvas", {
+      height: 400,
+      width: 400,
+    });
+  };
+
+  useEffect(() => {
+    setCanvas(initCanvas());
+    fabric.Image.fromURL(
+      "https://img.freepik.com/free-photo/white-cloud-blue-sky_74190-2381.jpg",
+      (img) => {
+        canvas.backgroundImage = img;
+      }
+    );
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Test fabric canvas</h1>
+      <canvas id="canvas" />
     </div>
   );
 }
